d4011a908cfd3bc8a53de1b07415b81549d5e18d
Likosen94 <Likosen94@gmail.com>
2021-07-19 03:14:16 -0600

    fix render resolutions and animation mobile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,15 +8,37 @@ const Header = () => {
     return (
         <div className="header-container">
             <div className="header-wraper" style={{ backgroundImage: `url(${background})` }} />
-            <div className="overlay"/>
-            <Particles/>
+            <div className="overlay" />
+            <Particles
+                className="particles-canvas"
+                params={{
+                    particles: {
+                        number: {
+                            value: 30,
+                            density: {
+                                enable: true,
+                                value_area: 900
+                            }
+                        },
+                        shape: {
+                            type: "",
+                            stroke: {
+                                width: 5,
+                                color: "f9ab00"
+                            }
+                        }
+                    }
+                }} />
             <div className="main-info">
-                <div className="info-content"> 
+                <div className="info-content">
                     <h1>
                         Yeiner Villegas, Software Developer
                     </h1>
+                    <p>
+                        Skills:
+                    </p>
                     <Rtype className="typed-text"
-                        strings={["JavaScript","React", "Java", "Ruby", "Bootstrap", "HTML", "CSS3"]}
+                        strings={["JavaScript", "React", "Java", "Ruby", "Bootstrap", "HTML", "CSS3"]}
                         typeSpeed={40}
                         backSpeed={60}
                         loop
