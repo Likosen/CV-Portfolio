48562408caa2ed51fc09b8211da9e5fcecdfe7c9
Likosen94 <Likosen94@gmail.com>
2021-07-19 01:55:28 -0600

    ico and other stuf

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./components/Nav/Navbar";
 import Header from "./components/Header/Header";
-import Particles from "react-particles-js";
 
 function App() {
   return (
